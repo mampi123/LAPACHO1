0ae3ce4d39fab0ebad725223652a4685b91b84af
Martina Mampel <mampelmartina@gmail.com>
2025-05-22 20:44:11 -0300

    function2

--- Php/app/public/wp-content/themes/themes/astra/functions.php
+++ Php/app/public/wp-content/themes/themes/astra/functions.php
@@ -272,6 +272,29 @@ function manejar_preflight() {
 }
 add_action('init', 'manejar_preflight');
 
+add_action('send_headers', function() {
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        $origin = $_SERVER['HTTP_ORIGIN'];
+        $allowed_origins = [
+            'https://www.talabarterialapacho.com',
+            'https://admin.talabarterialapacho.com',
+        ];
+        if (in_array($origin, $allowed_origins)) {
+            header("Access-Control-Allow-Origin: $origin");
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            header("Access-Control-Allow-Headers: Authorization, Content-Type");
+            header("Access-Control-Allow-Credentials: true");
+        }
+    }
+});
+
+add_action('init', function() {
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        status_header(200);
+        exit();
+    }
+});
+
 ?>
 
 
